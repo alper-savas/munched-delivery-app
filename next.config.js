707a612ca142d630b961d6bfd4b1b517f943bd70
alper-savas <alpersavas1998@gmail.com>
2023-08-29 23:14:19 +0200

    bugfix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    SECRET: "c0d0c6f45bd83088021668c156c6df5c",
+  },
 };
 
 module.exports = nextConfig;
