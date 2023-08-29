f0e6c6375e8d7330f02a8387a02749f9c2f27bdf
alper-savas <alpersavas1998@gmail.com>
2023-08-30 00:24:01 +0200

    Update .env

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXTAUTH_SECRET: "Ey7nTKnggBc0bRN8WUjyShw2qzOZ6KW4fUyqcKBePxY=",
-  },
 };
 
 module.exports = nextConfig;
