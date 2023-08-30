9d3973dccdfca553f93a90c01f6766a99acc17f2
alper-savas <alpersavas1998@gmail.com>
2023-08-30 03:06:48 +0200

    .env

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    // NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   },
 };
 
